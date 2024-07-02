42f95618af26004a01a47225391bf1dba3417bfc
dominic6809 <muuomusyoki018@gmail.com>
2024-07-02 13:59:55 -0700

    puppet file

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,19 +1,58 @@
+# Install nginx package
 package { 'nginx':
   ensure => installed,
 }
 
-file_line { 'aaaaa':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-}
-
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
-}
-
+# Ensure nginx service is running and enabled
 service { 'nginx':
-  ensure  => running,
+  ensure     => running,
+  enable     => true,
+  subscribe  => File['/etc/nginx/sites-available/default'],
+}
+
+# Create a custom index.html
+file { '/var/www/html/index.html':
+  ensure  => file,
+  content => 'Hello World!',
+  require => Package['nginx'],
+}
+
+# Backup the default site configuration file
+file { '/etc/nginx/sites-available/default.bak':
+  ensure  => file,
+  content => file('/etc/nginx/sites-available/default'),
+  require => Package['nginx'],
+}
+
+# Configure Nginx to set up the redirect and serve the custom page
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => template('nginx/default.erb'),
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+# Define the Nginx configuration template
+file { '/etc/puppetlabs/code/environments/production/modules/nginx/templates/default.erb':
+  ensure  => file,
+  content => '
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    root /var/www/html;
+    index index.html index.htm;
+
+    server_name _;
+
+    location / {
+        try_files $uri $uri/ =404;
+    }
+
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+}
+',
   require => Package['nginx'],
 }
