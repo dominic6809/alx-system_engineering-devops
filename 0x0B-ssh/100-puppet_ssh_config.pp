064736a0c0fa9bb74090f0053898c3f09b88dec0
dominic6809 <muuomusyoki018@gmail.com>
2024-08-22 05:36:02 -0700

    client configuration file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,18 +2,18 @@
 # using Puppet to make changes to our configuration file.
 
 file {'/etc/ssh/ssh_config';
-  ensure => 'present',
+  ensure  => 'present',
 }
 file_line {'turn off password authentication';
 }
-  path	=> '/etc/ssh/ssh_config',
-  line	=> 'PasswordAuthentication no',
-  match	=> 'PasswordAuthentication yes',
-  replace	=> 'true',
+  path    => '/etc/ssh/ssh_config',
+  line    => 'PasswordAuthentication no',
+  match   => 'PasswordAuthentication yes',
+  replace => 'true',
 
 file_line {'use an Identity file';
-  path    => '/etc/ssh/ssh_config',
-  line    => 'IdentityFile ~/.ssh/config',
-  match   => '^IdentityFile',
   ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/config',
+  match  => '^IdentityFile',
 }
