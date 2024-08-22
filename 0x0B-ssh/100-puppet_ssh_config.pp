b723d10baae0f12f1a01ef3996521bfd221b3cd7
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-08-22 15:49:06 +0300

    client config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -10,8 +10,8 @@ file_line {'turn off password auth';
         replace => 'true',
 }
 file_line {'Declare an Identity file';
-        ensure => 'present',
         path   => '/etc/ssh/ssh_config',
         line   => 'IdentityFile ~/.ssh/school',
         match  => '^IdentityFile',
+        ensure => 'present',
 }
