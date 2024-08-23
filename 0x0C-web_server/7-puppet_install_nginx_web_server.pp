3b14fe33ab7c2c89a50b97934f64aa15c0c3346d
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-08-23 12:43:21 +0300

    automate nginx configurations with puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,22 +1,39 @@
-# Automating the web configurations with puppet
+# Automating the Nginx configurations with puppet
 
-# install Nginx with puppet
-package { 'nginx';
+package { 'nginx':
   ensure => installed,
 }
 
-file_line { 'install';
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/@dominicmusyoki804 permanent;',
+file { '/var/www/html/index.html':
+  ensure  => file,
+  content => 'Hello World!',
 }
 
-file { '/var/www/html/index.html';
-  content  => 'Hello World!',
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => "
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    root /var/www/html;
+    index index.html;
+
+    server_name _;
+
+    location = /redirect_me {
+        return 301 https://www.youtube.com/@dominicmusyoki804;
+    }
+
+    location = / {
+        try_files \$uri \$uri/ =404;
+    }
+}
+  ",
+  notify  => Service[ 'nginx' ],
 }
 
-service { 'nginx';
-  ensure  => running,
-  require =>Package['nginx'],
+service { 'nginx':
+  ensure => running,
+  enable => true,
 }
