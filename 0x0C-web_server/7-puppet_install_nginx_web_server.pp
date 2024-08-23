b3a7f4c0940daa2a49d51a766cd0aac853f6886a
dominic6809 <muuomusyoki018@gmail.com>
2024-08-23 02:40:14 -0700

    automate with puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,58 +1,22 @@
-# Install nginx package
-package { 'nginx':
+# Automating the web configurations with puppet
+
+# install Nginx with puppet
+package { 'nginx';
   ensure => installed,
 }
 
-# Ensure nginx service is running and enabled
-service { 'nginx':
-  ensure     => running,
-  enable     => true,
-  subscribe  => File['/etc/nginx/sites-available/default'],
+file_line { 'install';
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/@dominicmusyoki804 permanent;',
 }
 
-# Create a custom index.html
-file { '/var/www/html/index.html':
-  ensure  => file,
-  content => 'Hello World!',
-  require => Package['nginx'],
+file { '/var/www/html/index.html';
+  content  => 'Hello World!',
 }
 
-# Backup the default site configuration file
-file { '/etc/nginx/sites-available/default.bak':
-  ensure  => file,
-  content => file('/etc/nginx/sites-available/default'),
-  require => Package['nginx'],
-}
-
-# Configure Nginx to set up the redirect and serve the custom page
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => template('nginx/default.erb'),
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-# Define the Nginx configuration template
-file { '/etc/puppetlabs/code/environments/production/modules/nginx/templates/default.erb':
-  ensure  => file,
-  content => '
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-
-    root /var/www/html;
-    index index.html index.htm;
-
-    server_name _;
-
-    location / {
-        try_files $uri $uri/ =404;
-    }
-
-    location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    }
-}
-',
-  require => Package['nginx'],
+service { 'nginx';
+  ensure  => running,
+  require =>Package['nginx'],
 }
