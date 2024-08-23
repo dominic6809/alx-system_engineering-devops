c6fbd7e1b6841a5971689d72114bbb31c8ffda98
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-08-23 12:55:28 +0300

    puppet_install_nginx_web_server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,36 +4,18 @@ package { 'nginx':
   ensure => installed,
 }
 
+file_line { 'set 301 redirection':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://github.com/dominic6809 permanent;',
+}
+
 file { '/var/www/html/index.html':
-  ensure  => file,
   content => 'Hello World!',
 }
 
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => "
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-
-    root /var/www/html;
-    index index.html;
-
-    server_name _;
-
-    location = /redirect_me {
-        return 301 https://www.youtube.com/@dominicmusyoki804;
-    }
-
-    location = / {
-        try_files \$uri \$uri/ =404;
-    }
-}
-  ",
-  notify  => Service[ 'nginx' ],
-}
-
 service { 'nginx':
-  ensure => running,
-  enable => true,
+  ensure  => running,
+  require => Package['nginx'],
 }
