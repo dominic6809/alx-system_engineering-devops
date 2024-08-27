3208437f5bda21a4ae0cd9952c377f289cf14442
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-08-27 11:07:08 +0300

    Update 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 """
-Script that returns info about an employee's TODO list progress and exports the data to a JSON file.
-Uses https://jsonplaceholder.typicode.com.
+Script that returns info about an employee's TODO list progress and exports 
+the data to a JSON file. Uses https://jsonplaceholder.typicode.com.
 """
 
 import json
 import requests
 from sys import argv
 
+
 def get_employee_todos_progress(employee_id):
     """
-    Fetch and display the TODO list progress of an employee. Also, export the data to a JSON file.
+    Fetch and display the TODO list progress of an employee. Also, export the 
+    data to a JSON file.
 
     Args:
         employee_id (int): The ID of the employee.
@@ -36,7 +38,10 @@ def get_employee_todos_progress(employee_id):
         completed_tasks = [task for task in todos_list if task['completed']]
         num_completed_tasks = len(completed_tasks)
         
-        print(f"Employee {employee_name} is done with tasks({num_completed_tasks}/{total_tasks}):")
+        print(
+            f"Employee {employee_name} is done with tasks({num_completed_tasks}/"
+            f"{total_tasks}):"
+        )
         for task in completed_tasks:
             print(f"\t {task['title']}")
         
@@ -58,10 +63,11 @@ def get_employee_todos_progress(employee_id):
             json.dump(json_data, json_file, indent=4)
         
         print(f"Data exported to {json_filename}")
-        
+
     except Exception as e:
         # Handle any errors that occur during the API requests
         print(f"An error occurred: {e}")
+
 
 if __name__ == "__main__":
     # Ensure the script is called with the correct number of arguments
